80fffa555a117a21ec5344d17bde43b1628f26db
Chudnovskyi <chudnovskiy0@gmail.com>
2024-02-06 23:58:06 +0100

    spring project init

---
+++ src/main/java/com/template/Entrypoint.java
@@ -0,0 +1,12 @@
+package com.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Entrypoint {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Entrypoint.class, args);
+    }
+}
