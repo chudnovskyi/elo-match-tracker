41aad49115f581c17c6617c5d87cbedea7e73e20
Mykola Yefremov <efremovnikolaus@gmail.com>
2024-11-24 01:25:44 +0100

    fix: IT and recalculation logic of subsequent matches

--- src/main/java/com/emt/repository/MatchRepository.java
+++ src/main/java/com/emt/repository/MatchRepository.java
@@ -6,18 +6,20 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MatchRepository extends JpaRepository<Match, Long> {
 
   @Query(
       """
-              SELECT m
-              FROM Match m
-              WHERE m.createdAt > :createdAt
-                AND (m.winner.playerId IN (:winnerId, :loserId)
-                     OR m.loser.playerId IN (:winnerId, :loserId))
-              ORDER BY m.createdAt
-              """)
+                  SELECT m
+                  FROM Match m
+                  WHERE m.createdAt > :createdAt
+                    AND (m.winner.playerId IN (:winnerId, :loserId)
+                         OR m.loser.playerId IN (:winnerId, :loserId))
+                  ORDER BY m.createdAt
+                  """)
   List<Match> findMatchesByPlayersAfter(
       @Param("createdAt") Instant createdAt,
       @Param("winnerId") Long winnerId,
